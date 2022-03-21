e191f7725e7641b56fbe942831fdab493d67eb17
Andrey <korandrey2@gmail.com>
2022-03-21 21:21:35 +0300

    Поправил замечания наставника

--- js/full-screen-photo.js
+++ js/full-screen-photo.js
@@ -1,10 +1,25 @@
 import {photosData} from './data.js';
 
-const keyCodeEsc = 27;
+const KEY_CODE_ESC = 27;
 const sectionFullSizePictures = document.querySelector('.big-picture');
 const showBigPhoto = sectionFullSizePictures.querySelector('.big-picture__img');
 const bigPictureCancel = sectionFullSizePictures.querySelector('.big-picture__cancel');
 const body = document.querySelector('body');
+const socialComments = sectionFullSizePictures.querySelector('.social__comments');
+const img = showBigPhoto.querySelector('img');
+const likesCount = sectionFullSizePictures.querySelector('.likes-count');
+const commentsCount = sectionFullSizePictures.querySelector('.comments-count');
+const socialCaption = sectionFullSizePictures.querySelector('.social__caption');
+const socialCommentCount = sectionFullSizePictures.querySelector('.social__comment-count');
+const commentsLoader = sectionFullSizePictures.querySelector('.comments-loader');
+const onButtonClose = () => {
+  closeFullScreenPhoto();
+};
+const onKeyCodeEsc = (event) => {
+  if (event.keyCode === KEY_CODE_ESC) {
+    closeFullScreenPhoto();
+  }
+};
 
 function closeFullScreenPhoto() {
   sectionFullSizePictures.classList.add('hidden');
@@ -15,38 +30,30 @@ function generateComments(smallPhoto) {
   const numberComments = smallPhoto.comments.length;
   let comments = '';
   for (let i = 0; i < numberComments; i++) {
-    comments += `<li class="social__comment">
-     <img
- class="social__picture"
-   src="${smallPhoto.comments[i].avatar}"
-   alt="${smallPhoto.comments[i].name}"
-   width="35" height="35">
-     <p class="social__text">${smallPhoto.comments[i].message}</p>
-     </li>`;
+    comments += `
+    <li class="social__comment">
+      <img
+      class="social__picture"
+      src="${smallPhoto.comments[i].avatar}"
+      alt="${smallPhoto.comments[i].name}"
+      width="35" height="35">
+      <p class="social__text">${smallPhoto.comments[i].message}</p>
+    </li>`;
   }
   return comments;
 }
 
 export function openFullScreenPhoto(smallPhoto) {
   sectionFullSizePictures.classList.remove('hidden');
-  showBigPhoto.querySelector('img').setAttribute('src', smallPhoto.url);
-  sectionFullSizePictures.querySelector('.likes-count').textContent = smallPhoto.likes;
-  sectionFullSizePictures.querySelector('.comments-count').textContent = smallPhoto.comments.length;
-  const socialComments = sectionFullSizePictures.querySelector('.social__comments');
+  img.setAttribute('src', smallPhoto.url);
+  likesCount.textContent = smallPhoto.likes;
+  commentsCount.textContent = smallPhoto.comments.length;
   socialComments.innerHTML = generateComments(smallPhoto);
-  sectionFullSizePictures.querySelector('.social__caption').textContent = smallPhoto.description;
-  sectionFullSizePictures.querySelector('.social__comment-count').classList.add('hidden');
-  sectionFullSizePictures.querySelector('.comments-loader').classList.add('hidden');
+  socialCaption.textContent = smallPhoto.description;
+  socialCommentCount.classList.add('hidden');
+  commentsLoader.classList.add('hidden');
   body.classList.add('modal-open');
-  const onButtonClose = () => {
-    closeFullScreenPhoto();
-  };
   bigPictureCancel.addEventListener('click', onButtonClose);
-  const onKeyCodeEsc = (event) => {
-    if (event.keyCode === keyCodeEsc) {
-      closeFullScreenPhoto();
-    }
-  };
   document.addEventListener('keydown', onKeyCodeEsc);
 }
 
